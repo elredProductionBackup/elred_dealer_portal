e41cd50da445d8611fc0c19489aee9bca205aeb9
nikita-dev <nikita.baraiya@devdigital.com>
2025-10-30 15:40:09 +0530

    Confirmation Popup

---
+++ components/ConfirmationPopup.jsx
@@ -0,0 +1,40 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import orderSuccess from "../assets/images/order-placed.svg";
+
+export default function ConfirmationPopup({ message, subheading }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-[#0000001A]">
+      <div className="bg-white rounded-2xl p-8 w-[500px] h-[500px] text-center py-[70px] px-[40px]">
+        <div className="flex justify-center mb-[20px]">
+         <Image
+            src={orderSuccess}   
+            alt="Order Placed"
+            width={218}                 
+            height={173}                
+            priority                   
+          />
+        </div>
+        <h2 className="text-xl font-semibold mb-[12px]">
+          {message}
+        </h2>
+        <p className="text-[#909090] text-[14px] mb-[30]">
+          {subheading}
+        </p>
+        <a
+          onClick={() => {
+            setIsOpen(false);
+          }}
+          className="inline-block cursor-pointer bg-[#BE212A] text-white text-[18px] font-medium px-[40px] py-[10px] rounded-[12px]"
+        >
+          View Order
+        </a>
+      </div>
+    </div>
+  );
+}
