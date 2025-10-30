e218d4c5f466ea1b47da797beb23044790d7bc73
nikita-dev <nikita.baraiya@devdigital.com>
2025-10-30 18:02:39 +0530

    Product Image component

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,7 +1,19 @@
 import ConfirmationPopup from "../../../components/ConfirmationPopup";
+import ProductImage from "../../../components/ProductImage";
+import product1 from "../../../assets/images/product-1.png";
+import product2 from "../../../assets/images/product-2.png";
+import product3 from "../../../assets/images/product-3.png";
 
 export default function TestPage() {
+  const productImages = [product1, product2, product3];
   return (
-    <ConfirmationPopup message="Your order has been placed successfully !!!" subheading="You will be informed by your partner once the order is dispatched." />
+    <main>
+      <ConfirmationPopup
+        message="Your order has been placed successfully !!!"
+        subheading="You will be informed by your partner once the order is dispatched."
+      />
+
+      <ProductImage images={productImages} />
+    </main>
   );
 }
